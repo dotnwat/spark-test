42cc9fc8c51f51ad015f337ab5c71230286ea7aa
Noah Watkins <noahwatkins@gmail.com>
2016-05-02 16:25:31 -0700

    fixes
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- spark-perf/run.sh
+++ spark-perf/run.sh
@@ -1,10 +1,21 @@
 #!/bin/bash
 
+set -e
+set -x
+
 cdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 docker build -t spark-perf $cdir
 
+if [ -d results ]; then
+  mv results results.prev
+fi
+
+mkdir results
+
 # -it --entrypoint=/bin/bash \
 docker run \
+  --rm -it \
   -v $cdir/config.py.tiny:/spark-perf/config/config.py \
+  -v `pwd`/results:/spark-perf/results \
   spark-perf bin/run
