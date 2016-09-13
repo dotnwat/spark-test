eb57beb07412b905ca9537a9ac9361a97bb59755
Noah Watkins <noahwatkins@gmail.com>
2016-09-13 09:50:30 -0700

    enable event logging
    Signed-off-by: Noah Watkins <noahwatkins@gmail.com>

--- spark-perf/run.sh
+++ spark-perf/run.sh
@@ -19,16 +19,24 @@ outdir=results.${EXPR}.date-$datestr
 mkdir $outdir
 
 logdir=logs.${EXPR}.date-$datestr
-mkdir $logdir
+mkdir -p $logdir/eventlogs
+
+function finish {
+  docker kill $1 || true
+  docker rm $1 || true
+}
 
 cid=$(docker run \
   -d -it \
   -v `pwd`/$EXPR/config.py:/spark-perf/config/config.py \
   -v `pwd`/$EXPR/spark-env.sh:/spark/conf/spark-env.sh \
+  -v `pwd`/$EXPR/spark-defaults.conf:/spark/conf/spark-defaults.conf \
   -v `pwd`/slaves:/spark/conf/slaves \
   -v `pwd`/$outdir:/spark-perf/results \
   -v `pwd`/$logdir:/spark/logs \
   spark-perf bin/run)
+
+trap 'finish $cid' EXIT
 
 while true; do
   RUNNING=$(docker inspect --format="{{ .State.Running }}" $cid 2> /dev/null)
@@ -43,5 +51,3 @@ while true; do
 
   sleep 2
 done
-
-docker rm $cid
